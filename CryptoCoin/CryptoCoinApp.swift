5042a25d691f89ae11020f5968656e5aa3ed1067
yyeonjju <llilyha123@gmail.com>
2024-09-06 14:30:52 +0900

    Initial Commit

---
+++ CryptoCoin/CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinApp.swift
+//  CryptoCoin
+//
+//  Created by 하연주 on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
